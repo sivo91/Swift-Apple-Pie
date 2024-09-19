997a153b826d99ad05bbe3ddaf5a6e7d3c90e2b3
Peter <83480296+sivo91@users.noreply.github.com>
2024-09-19 09:58:31 -0700

    Add files via upload

---
+++ Game.swift
@@ -0,0 +1,32 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Peter Sivak on 9/17/24.
+//
+
+import Foundation
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
